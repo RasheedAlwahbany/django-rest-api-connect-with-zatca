1286b4ab4d8e5fc1e9f730e8777d7d27b5c009c0
RasheedAlwahbanyMinsati <rasheed.alwahbany@minsati.com>
2022-11-06 14:31:09 +0300

    Initial app

---
+++ api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .import views
+from rest_framework.urlpatterns import format_suffix_patterns
+
+urlpatterns = [
+     path('api/',views.OutAPIData.as_view()),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
