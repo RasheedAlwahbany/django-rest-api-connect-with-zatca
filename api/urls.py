88085a123e9da1119e1135655a13b59c3b859962
RasheedAlwahbanyMinsati <rasheed.alwahbany@minsati.com>
2022-11-06 16:09:20 +0300

    Complete connecting with ZATCA API for all 6 phases

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from .import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-     path('api/',views.OutAPIData.as_view()),
+     path('api/',views.ZATCAAPI.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
